e9c010f2e7553b7325d410a50f25fb0dcb4d8206
Shashank Shovit <shanky@vm.localdomain>
2022-05-22 11:29:24 +0530

    Port modified for heroku

--- start.py
+++ start.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, request, url_for
 
 import face_recognition
 import json
+import os
 
 app = Flask(__name__)
 
@@ -44,4 +45,5 @@ def error():
 	return render_template("error.html")
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
